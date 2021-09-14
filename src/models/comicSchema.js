b12e47f9ae664e63730c7d731f73de81461f8c93
VulpesVeritas <66179575+VulpesVeritas@users.noreply.github.com>
2021-09-14 18:44:10 -0500

    Creacion de backend
    Este es el inicio del backend y del proyecto:
    -Agregado index
    -Agregado Esquemas
    -Agregado Controladores
    -Agregado Rutas

---
+++ src/models/comicSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const ComicSchema = new mongoose.Schema({ //Aun no lo termino
+    titulo: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 75,
+        unique:true
+    },
+    idCreador:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "usuario",
+        required: true
+    },
+    calificacion: { //este me da cosa...creo que deberia ser un arreglo o deberiamos de hacer otra tabla con calificaciones solas...
+        type: Number,
+        default: 0
+    },
+    tipo: [{ //no terminado
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tipo",
+        required: true
+    }]
+})
+
+const Comic= mongoose.model('comic', ComicSchema); 
+module.exports = Comic; 
