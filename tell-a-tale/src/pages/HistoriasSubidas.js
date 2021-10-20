70fca7d52e9a29d5cd82059623b0a01eef9254e8
NathFromLuna <natalysc366@gmail.com>
2021-10-20 17:53:10 -0500

    un poco mas de avance

--- tell-a-tale/src/pages/HistoriasSubidas.js
+++ tell-a-tale/src/pages/HistoriasSubidas.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SimplePaper from '../components/CuadrosNotificaciones'
 
 export default function HistoriasSubidas() {
     return (
         <div>
-            
+            <SimplePaper></SimplePaper>
+            <SimplePaper></SimplePaper>
+            <SimplePaper></SimplePaper>
         </div>
     )
 }
--- tell-a-tale/src/pages/SubirComic.js
+++ tell-a-tale/src/pages/SubirComic.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import ComboBox from '../components/ComboBox'
 
 export default function SubirComic() {
     return (
         <div>
+            <div>
+                <image src='./imagenes/goreAgony2'>
+
+                </image>
+                <image src='./imagenes/goreAgony2'>
+
+                </image>
+            </div>
+            <div>
+            <ComboBox>Genero 1</ComboBox>
+            <ComboBox>Genero 1</ComboBox>
+            </div>
+            <div>
+                <label>
+                    Titulo
+                </label>
+                <input type='text'></input>
+                <label>
+                    Descripcion
+                </label>
+                <textarea></textarea>
+            </div>
             
         </div>
     )
