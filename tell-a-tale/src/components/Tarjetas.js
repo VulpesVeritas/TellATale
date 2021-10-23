4859cde067a45738b6c9ba330af49b4ca9d8c8eb
Fabiola <fabiola-2803@hotmail.com>
2021-10-23 05:12:37 -0500

    tarjetas

--- tell-a-tale/src/components/Tarjetas.js
+++ tell-a-tale/src/components/Tarjetas.js
@@ -16,10 +16,10 @@ export default function ActionAreaCard() {
           alt="Agony"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography class = "text-card text-card-principal" gutterBottom variant="h5" component="div">
             Lizard
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography class = "text-card" variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography>
