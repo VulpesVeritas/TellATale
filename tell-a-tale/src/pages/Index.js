568ca71af3828650db3968fd413e6a5ff245d1c4
Fabiola <fabiola-2803@hotmail.com>
2021-10-23 04:31:16 -0500

    slide

--- tell-a-tale/src/pages/Index.js
+++ tell-a-tale/src/pages/Index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ActionAreaCard from '../components/Tarjetas'
-import BasicRating from '../components/Calificacion'
+//import BasicRating from '../components/Calificacion'
 import '../css/tarjetas.css'
+import Slide from '../components/Slide'
 
 export default function Index() {
     return (
         <div className='contenedorPrinc'>
+            <Slide></Slide>
                 <div className='carrusel'>
                 
                 </div>
