deff63affe5261acca6be514d748d9ce0c8a0f6f
Fabiola <fabiola-2803@hotmail.com>
2021-11-12 23:09:35 -0600

    Enviar datos
    de index a tarjetas

--- tell-a-tale/src/components/Tarjetas.js
+++ tell-a-tale/src/components/Tarjetas.js
@@ -6,25 +6,24 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import '../css/tarjetas.css'
 
-export default function ActionAreaCard() {
+export default function ActionAreaCard(props) {
   return (
     <Card class="cardMod" sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           class="IMGCard"
           component="img"
-          image="https://pbs.twimg.com/media/E9vwPTbX0AE3r0I?format=jpg&name=large"
+          image={props.imagen}
           alt="green iguana"
         />
         <CardContent
           class="ContentCard"
           >
           <Typography class="Title" gutterBottom variant="h5" component="div">
-            Lizard
+            {props.titulo}
           </Typography>
           <Typography class="Desc" variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {props.descripcion}
           </Typography>
         </CardContent>
       </CardActionArea>
--- tell-a-tale/src/pages/SubirComic.js
+++ tell-a-tale/src/pages/SubirComic.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ComboBox from '../components/ComboBox'
 import '../css/subircomic.css'
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 
 export default function SubirComic() {
     return (
