1c1ea5935bc3bfe0c64ec7a7b010006ea4445eb0
Fabiola <fabiola-2803@hotmail.com>
2021-11-14 22:02:35 -0600

    Registrarse

--- tell-a-tale/src/App.js
+++ tell-a-tale/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import PrimarySearchAppBar from './components/TopBar';
 import Index from './pages/Index';
+import IniciarSesion from './pages/IniciarSesion';
+import Registrarse from './pages/Registrarse';
 import Favoritos from './pages/Favoritos';
 import Ajustes from './pages/Ajustes';
 import Comic from './pages/Comic'
@@ -21,12 +23,18 @@ function App() {
         <PrimarySearchAppBar>
           <Switch>
             <Route exact path="/" component={Index} />
-            <Route exact path="/Favoritos" component={Favoritos} />
+            <Route exact path="/IniciarSesion" component={IniciarSesion} />
+            <Route exact path="/Registrarse" component={Registrarse} />
+
             <Route exact path="/CrearComic" component={CrearComic}/>
-            <Route exact path="/CrearCapComic" component={SubirCapComic}/>
-            <Route exact path="/Perfil" component={HistoriasSubidas}/>
-            <Route exact path="/Comic" component={Comic}/>
             <Route exact path="/EditarComic" component={EditarComic}/>
+            <Route exact path="/CrearCapComic" component={SubirCapComic}/>
+
+            <Route exact path="/Perfil" component={HistoriasSubidas}/>
+            <Route exact path="/Favoritos" component={Favoritos} />
+
+            <Route exact path="/Comic" component={Comic}/>
+
             <Route exact path="/Ajustes" component={Ajustes}/>            
 
             <Route exact path="/Notificaciones" component={Notificaciones}/>
